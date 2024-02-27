6d15728d64028256da8d21f7469b43407b7ecf77
TerminaTerrier <ianmg211101@gmail.com>
2024-02-26 19:05:02 -0600

    switch to vs code

--- Components/signalbus.cs
+++ Components/signalbus.cs
@@ -3,17 +3,17 @@ using System;
 
 public partial class signalbus : Node
 {
-    [Signal]
-    public delegate void LifeLostEventHandler();
-    [Signal]
-    public delegate void LivesDepletedEventHandler();
-    [Signal]
-    public delegate void ItemCollectedEventHandler(StringName collectable);
-    [Signal]
-    public delegate void StateChangeEventHandler(string state);
-    [Signal]
-    public delegate void LeftWarpEventHandler(Node2D node);
-    [Signal]
-    public delegate void RightWarpEventHandler(Node2D node);
+	[Signal]
+	public delegate void LifeLostEventHandler();
+	[Signal]
+	public delegate void LivesDepletedEventHandler();
+	[Signal]
+	public delegate void ItemCollectedEventHandler(StringName collectable);
+	[Signal]
+	public delegate void StateChangeEventHandler(string state);
+	[Signal]
+	public delegate void LeftWarpEventHandler(Node2D node);
+	[Signal]
+	public delegate void RightWarpEventHandler(Node2D node);
 
 }
--- game_manager.cs
+++ game_manager.cs
@@ -3,27 +3,27 @@ using System;
 
 public partial class game_manager : Node2D
 {
-    [Signal]
-    public delegate void SaveDataEventHandler();
-    
+	[Signal]
+	public delegate void SaveDataEventHandler();
+	
 
-    public override void _Ready()
-    {
-        //healthComponent = GetNode<Node2D>("/root/Main/Player/HealthComponent");
-
-        // Connect(signalbus.SignalName.LifeLost, Callable.From(OnLifeLost));
-        //  Connect(signalbus.SignalName.LivesDepleted, Callable.From(OnLivesDepleted));
-        
-    }
-
-    public void OnLifeLost()
-    {
-       
-    }
-
-    public void OnLivesDepleted()
+	public override void _Ready()
 	{
-        EmitSignal(SignalName.SaveData);
-        GetTree().Paused = true;
-    }
+		//healthComponent = GetNode<Node2D>("/root/Main/Player/HealthComponent");
+
+		// Connect(signalbus.SignalName.LifeLost, Callable.From(OnLifeLost));
+		//  Connect(signalbus.SignalName.LivesDepleted, Callable.From(OnLivesDepleted));
+		
+	}
+
+	public void OnLifeLost()
+	{
+	   
+	}
+
+	public void OnLivesDepleted()
+	{
+		EmitSignal(SignalName.SaveData);
+		GetTree().Paused = true;
+	}
 }
